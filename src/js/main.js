62cf9e5667c40e8ae4ae2b55aaed6cf207476927
PavelSL <spav112@yandex.ru>
2022-09-25 15:21:59 +0300

    Video

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 import Slider from "./modules/slider";
+import VideoPlayer from "./modules/playVideo";
 
 window.addEventListener('DOMContentLoaded', () => {
     let slider = new Slider('.page', '.next').render();
+    let player = new VideoPlayer('.showup .play', '.overlay').init();
 });
